a0e85bb39ec41a461b5684664e28d2029a6ec6e5
DaiMysha <mlp.lemon.birdy@gmail.com>
2016-12-09 16:02:48 +0100

    Calculates normals and projections and prints them

--- SAT/sat/main.cpp
+++ SAT/sat/main.cpp
@@ -30,9 +30,15 @@ float getAngleBetweenVectors(const sf::Vector2<T>& o, const sf::Vector2<T>& v) {
 }
 
 template <typename T>
+float norm2(const sf::Vector2<T>& a)
+{
+    return static_cast<float>(a.x*a.x + a.y*a.y);
+}
+
+template <typename T>
 float norm(const sf::Vector2<T>& a)
 {
-    return sqrt(static_cast<float>(a.x*a.x + a.y*a.y));
+    return sqrt(norm2(a));
 }
 
 template <typename T>
@@ -40,6 +46,11 @@ sf::Vector2<T> normalize(const sf::Vector2<T>& vec)
 {
     T n = norm(vec);
     return sf::Vector2<T>(T(vec.x / n), T(vec.y / n));
+}
+
+template <typename T>
+T dot(const sf::Vector2<T>& a, const sf::Vector2<T>& b) {
+    return a.x*b.x + a.y*b.y;
 }
 
 class Axis : public sf::Drawable
@@ -67,6 +78,10 @@ class Segment : public sf::Drawable
         std::array<sf::Vector2f,2> points;
         sf::Color color;
 
+        Segment(const Axis& a) : Segment(a.origin, a.origin + a.direction, a.color)
+        {
+        }
+
         Segment(const sf::Vector2f& a = { 0, 0 }, const sf::Vector2f& b = { 0, 0 }, const sf::Color& c = sf::Color::Black)
         {
             points[0] = a;
@@ -89,27 +104,34 @@ class Segment : public sf::Drawable
             return points[1] - points[0];
         }
 
+        float norm()
+        {
+            return ::norm(points[1] - points[0]);
+        }
+
     protected:
         virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 };
 
 void calcNormals(std::array<sf::RectangleShape, 2>& boxes, std::array<Axis, 4>& normals)
 {
+
+    int colorReduction = 3;
     {
         sf::Transform t = boxes[0].getTransform();
         {
             Segment s(t.transformPoint(boxes[0].getPoint(0)), t.transformPoint(boxes[0].getPoint(1)), boxes[0].getFillColor());
-            s.color.r *= 0.5f;
-            s.color.g *= 0.5f;
-            s.color.b *= 0.5f;
+            s.color.r /= colorReduction;
+            s.color.g /= colorReduction;
+            s.color.b /= colorReduction;
             normals[0] = s.getNormal();
             normals[0].origin -= s.getDirection();
         }
         {
             Segment s(t.transformPoint(boxes[0].getPoint(1)), t.transformPoint(boxes[0].getPoint(2)), boxes[0].getFillColor());
-            s.color.r *= 0.5f;
-            s.color.g *= 0.5f;
-            s.color.b *= 0.5f;
+            s.color.r /= colorReduction;
+            s.color.g /= colorReduction;
+            s.color.b /= colorReduction;
             normals[1] = s.getNormal();
             normals[1].origin -= s.getDirection();
         }
@@ -119,19 +141,63 @@ void calcNormals(std::array<sf::RectangleShape, 2>& boxes, std::array<Axis, 4>& 
         sf::Transform t = boxes[1].getTransform();
         {
             Segment s(t.transformPoint(boxes[1].getPoint(0)), t.transformPoint(boxes[1].getPoint(1)), boxes[1].getFillColor());
-            s.color.r *= 0.5f;
-            s.color.g *= 0.5f;
-            s.color.b *= 0.5f;
+            s.color.r /= colorReduction;
+            s.color.g /= colorReduction;
+            s.color.b /= colorReduction;
             normals[2] = s.getNormal();
             normals[2].origin += s.getDirection();
         }
         {
             Segment s(t.transformPoint(boxes[1].getPoint(1)), t.transformPoint(boxes[1].getPoint(2)), boxes[1].getFillColor());
-            s.color.r *= 0.5f;
-            s.color.g *= 0.5f;
-            s.color.b *= 0.5f;
+            s.color.r /= colorReduction;
+            s.color.g /= colorReduction;
+            s.color.b /= colorReduction;
             normals[3] = s.getNormal();
             normals[3].origin += s.getDirection();
+        }
+    }
+}
+
+//returns distance from axis center to point
+float project(const sf::Vector2f& v, const Axis& axis)
+{
+
+    return dot((v-axis.origin), axis.direction);
+}
+
+Segment project(const sf::RectangleShape& box, const Axis& axis)
+{
+    const sf::Transform& t = box.getTransform();
+    std::array<float, 4> projections;
+    for (size_t i = 0; i < 4; ++i)
+    {
+        projections[i] = project(t.transformPoint(box.getPoint(i)), axis);
+    }
+
+    float mini = projections[0], maxi = projections[0];
+    for (size_t i = 1; i < 4; ++i)
+    {
+        if (projections[i] < mini)
+        {
+            mini = projections[i];
+        }
+        else if(projections[i] > maxi)
+        {
+            maxi = projections[i];
+        }
+    }
+
+    return Segment(axis.origin + mini * axis.direction, axis.origin + maxi * axis.direction, sf::Color::White);
+}
+
+void calcProjections(std::array<Segment, 8>& projections, const std::array<sf::RectangleShape, 2>& boxes, const std::array<Axis, 4>& normals)
+{
+    for (size_t b = 0; b < boxes.size(); ++b)
+    {
+        for (size_t n = 0; n < normals.size(); ++n)
+        {
+            projections[b * 4 + n] = project(boxes[b], normals[n]);
+            projections[b * 4 + n].color = boxes[b].getFillColor();
         }
     }
 }
@@ -145,7 +211,7 @@ int main(int argc, char** argv)
 
     std::array<sf::RectangleShape, 2> boxes;
     boxes[0].setFillColor(sf::Color::Blue);
-    boxes[0].setPosition(150, 50);
+    boxes[0].setPosition(150, 150);
     boxes[0].setSize({ 150, 50 });
     boxes[0].setRotation(30);
 
@@ -167,8 +233,10 @@ int main(int argc, char** argv)
 
     //SAT related stuff
     std::array<Axis, 4> normals;
+    std::array<Segment, 8> projections;
 
     calcNormals(boxes, normals);
+    calcProjections(projections, boxes, normals);
 
     //the loop
     while (window.isOpen())
@@ -238,6 +306,7 @@ int main(int argc, char** argv)
                         boxes[affectedBox].setRotation(angle * 180.0f / (float)PI);
                     }
                     calcNormals(boxes, normals);
+                    calcProjections(projections, boxes, normals);
                 }
             }
         }
@@ -250,6 +319,10 @@ int main(int argc, char** argv)
         for (size_t i = 0; i < normals.size(); ++i)
         {
             window.draw(normals[i]);
+        }
+        for (size_t i = 0; i < projections.size(); ++i)
+        {
+            window.draw(projections[i]);
         }
         window.display();
 
